2de2b5746f423272c3f62c387ce8fc6ca69bcc78
Uthaeus <Uth@github.com>
2018-01-11 21:29:35 -0700

    Implemented comments, added user to seeds, and refactored blogs page to render partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
+User.create!(
+    email: "test2@test.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    name: "User",
+    roles: "Regular User"
+)
+
+puts "1 Regular User created"
+
+User.create!(
+    email: "test@test.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    name: "testuser",
+    roles: "site_admin"
+)
+
+puts "1 Admin User created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Whole bunch of stuff here to look like a blog post.",
+    body: "Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.",
     )
 end
 
